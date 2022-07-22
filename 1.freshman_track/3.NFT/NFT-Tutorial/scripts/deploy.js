3759bcca8c1a9e0a20185daa1fd728022ef46c55
Alvo Rithm <alvorithmemail@gmail.com>
2022-07-22 19:24:18 +0200

    smart contract address: 0x1d61c0ae11742615F37d8fF578b01DC404C70Dc1

--- 1.freshman_track/3.NFT/NFT-Tutorial/scripts/deploy.js
+++ 1.freshman_track/3.NFT/NFT-Tutorial/scripts/deploy.js
@@ -1,4 +1,4 @@
-// We require the Hardhat Runtime Environment explicitly here. This is optional
+/*// We require the Hardhat Runtime Environment explicitly here. This is optional
 // but useful for running the script in a standalone fashion through `node <script>`.
 //
 // You can also run a script with `npx hardhat run <script>`. If you do that, Hardhat
@@ -27,3 +27,28 @@ main().catch((error) => {
   console.error(error);
   process.exitCode = 1;
 });
+*/
+// Import ethers from Hardhat package
+const { ethers } = require("hardhat");
+
+async function main() {
+  /*
+A ContractFactory in ethers.js is an abstraction used to deploy new smart contracts,
+so nftContract here is a factory for instances of our GameItem contract.
+*/
+  const nftContract = await ethers.getContractFactory("GameItem");
+
+  // here we deploy the contract
+  const deployedNFTContract = await nftContract.deploy();
+
+  // print the address of the deployed contract
+  console.log("NFT Contract Address:", deployedNFTContract.address);
+}
+
+// Call the main function and catch if there is any error
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
