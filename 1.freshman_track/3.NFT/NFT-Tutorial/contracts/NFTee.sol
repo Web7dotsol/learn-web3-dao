b8342ac19c1dfdaed61500b4261653d05502033b
Alvo Rithm <alvorithmemail@gmail.com>
2022-07-23 18:07:00 +0200

    removed json file

--- 1.freshman_track/3.NFT/NFT-Tutorial/contracts/NFTee.sol
+++ 1.freshman_track/3.NFT/NFT-Tutorial/contracts/NFTee.sol
@@ -13,3 +13,20 @@ contract GameItem is ERC721 {
         _mint(msg.sender, 1);
     }
 }
+
+/*
+
+$ nvm use 16
+Now using node v16.9.1 (npm v7.21.1)
+$ node -v
+v16.9.1
+$ nvm use 14
+Now using node v14.18.0 (npm v6.14.15)
+$ node -v
+v14.18.0
+$ nvm install 12
+Now using node v12.22.6 (npm v6.14.5)
+$ node -v
+v12.22.6
+
+*/
