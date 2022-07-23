0a82b4ce63359d8bc126b15ba33245f6b0b91ce6
Alvo Rithm <alvorithmemail@gmail.com>
2022-07-23 10:46:10 +0200

    added in hardhat-ethers plugin and waffles plugin using --save-dev command - package.json file updated automatically

--- 1.freshman_track/3.NFT/NFT-Tutorial/hardhat.config.js
+++ 1.freshman_track/3.NFT/NFT-Tutorial/hardhat.config.js
@@ -1,5 +1,10 @@
 require("@nomicfoundation/hardhat-toolbox");
+require("@nomiclabs/hardhat-ethers");
+require("@nomiclabs/hardhat-waffle");
 require("dotenv").config({ path: ".env" });
+require("@nomiclabs/hardhat-etherscan");
+let secrets = require("./secrets");
+
 
 const ALCHEMY_API_KEY_URL = process.env.ALCHEMY_API_KEY_URL;
 const RINKEBY_PRIVATE_KEY = process.env.RINKEBY_PRIVATE_KEY;
@@ -12,4 +17,7 @@ module.exports = {
       accounts: [RINKEBY_PRIVATE_KEY],
     },
   },
+  etherscan: {
+    apiKey: "https://api-rinkeby.etherscan.io/RSDTJGTGW5U2T2NH7MIRJJEQ5AEMB4IB8N"
+  }
 };
