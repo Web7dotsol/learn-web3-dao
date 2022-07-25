672cbf660a209e82459640ae699582c8bfb98051
Alvo Rithm <alvorithmemail@gmail.com>
2022-07-25 21:11:10 +0200

    inheritance lesson completed

--- 2.Sophomore_track/1.0.Advanced-Solidity/8.0.Inheritance.sol
+++ 2.Sophomore_track/1.0.Advanced-Solidity/8.0.Inheritance.sol
@@ -12,5 +12,64 @@ pragma solidity ^0.8.10;
 // must use the 'override' keyword.
 // The order of inheritance matters if parent contracts share methods
 // or attributes by the same name
+
+/* Graph of inheritance
+    A
+   / \
+  B   C
+ / \ /
+F  D,E
+
+*/
 contract A {
+    function foo() public pure virtual returns (string memory) {
+        return "A";
+    }
+}
+
+// Contract B inherit other contracts y using the keyword 'is'
+contract B is A {
+    // Override A.foo()
+    // But also allow this function to be overridden by further children
+    // S0 we specify both keywords - virtual and override
+    function foo() public pure virtual override returns (string memory){
+        return "B";
+    }
+}
+
+contract C is A {
+    // Similar to contract B above
+    function foo() public pure virtual override returns (string memory) {
+        return "C";
+    }
+}
+
+// Contracts can inherit from multiple parent contracts.
+// When a function is called that is defined mutlple times in
+// different contracts, parent contracts are searched from
+// right to left, and in depth-first manner
+
+contract D is B, C {
+    // D.foo() returns "B"
+    // since B is the right most parent contract with function foo()
+    function foo() public pure override(B, C) returns (string memory) {
+        return super.foo();
+    }
+}
+
+contract E is C, B {
+    // E.foo() returns "B
+    // since B is the right most parent contract with function foo()
+    function foo() public pure override(C, B) returns (string memory) {
+        return super.foo();
+    }
+
+// Inheritance must be ordered from "most base-like" to "most derived"
+// Swapping the order of A and B will throw a compilation error.
+// contract F is A, B {
+//    function foo() public pure override(A, B) returns (string memory) {
+//        return super.foo();
+//    }
+}
+
    
