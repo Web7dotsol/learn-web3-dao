2c3c6faeb82ee88acbf302478f363c339791213f
Alvo Rithm <alvorithmemail@gmail.com>
2022-07-25 21:46:10 +0200

    calling external functions lesson complete

--- 2.Sophomore_track/1.0.Advanced-Solidity/10.0.CallingExternalContracts.sol
+++ 2.Sophomore_track/1.0.Advanced-Solidity/10.0.CallingExternalContracts.sol
@@ -1,21 +1,44 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.10;
 
-// define the contract using contratc function
-contract MoodDiary {
-    // string variable to store mood
-    string public mood;
+// Calling External Contracts
 
-    // function to set the mood use _mood as a parameter to avoid 
-    // variable overshadowing
-    function setMood(string memory _mood) public {
-        mood = _mood;
+// Contracts can call other contracts by just calling functions on an 
+// instance of the other contract like A.foo(x, y, z). To do so, you 
+// must have an interface for A which tells your contract which 
+// functions exist. Interfaces in Solidity behave like header files, 
+// and serve similar purposes to the ABI we have been using when 
+// calling contracts from the frontend. This allows a contract to 
+// know how to encode and decode function arguments and return values 
+// for calling external contracts.
+
+//  Interfaces you use do not need to be extensive. i.e. they 
+// do not need to necessarily contain all the functions that exist 
+// in the external contract - only those which you might be calling 
+// at some point.
+
+// Assume there is an external ERC20 contract, and we are interested
+// in calling the balanceOf function to check the balance of a given 
+// address from our contract.
+
+interface MinimalERC20 {
+    // Just include the functions we are interested in
+    // in the interface
+    function balanceOf(address account) external view returns (uint256);
+}
+
+contract MyContract {
+    MinimalERC20 externalContract;
+
+    constructor(address _externalContract) {
+        // Initialize a MinimalERC20 contract instance
+        externalContract = MinimalERC20(_externalContract);
     }
 
-    // fucntion to detect the mood from the smart contract
-    // view only function that doe sinvoke blockchain changes
-    // caller requires no gas fees to execute the fucntion to read data
-    function getMood() public view returns(string memory) {
-        return mood;
+    function mustHaveSomeBalance() public {
+        //Require that the caller of this transaction has a non-zero
+        // balance of tokens in the external ERC20 contract
+        uint balance = externalContract.balanceOf(msg.sender);
+        require(balance > 0, "You don't own any tokens of external contract");
     }
 }
