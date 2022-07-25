a3e23f6ad819aa0446278485c08ad0042c1480d9
Alvo Rithm <alvorithmemail@gmail.com>
2022-07-25 22:16:03 +0200

    external import file updated

--- 2.Sophomore_track/1.0.Advanced-Solidity/11.2.External Imports.sol
+++ 2.Sophomore_track/1.0.Advanced-Solidity/11.2.External Imports.sol
@@ -2,20 +2,6 @@
 pragma solidity ^0.8.10;
 
 // define the contract using contratc function
-contract MoodDiary {
-    // string variable to store mood
-    string public mood;
-
-    // function to set the mood use _mood as a parameter to avoid 
-    // variable overshadowing
-    function setMood(string memory _mood) public {
-        mood = _mood;
-    }
-
-    // fucntion to detect the mood from the smart contract
-    // view only function that doe sinvoke blockchain changes
-    // caller requires no gas fees to execute the fucntion to read data
-    function getMood() public view returns(string memory) {
-        return mood;
-    }
+contract ExternalImports {
+    
 }
