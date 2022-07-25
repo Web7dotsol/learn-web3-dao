ea6203f0ab4fea748f960daf11681500b15dff7f
Alvo Rithm <alvorithmemail@gmail.com>
2022-07-25 18:05:01 +0200

    still busy with inheritance - will wrap up th lesson soon

--- 2.Sophomore_track/1.0.Advanced-Solidity/8.0.Inheritance.sol
+++ 2.Sophomore_track/1.0.Advanced-Solidity/8.0.Inheritance.sol
@@ -1,21 +1,16 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.10;
-
-// define the contract using contratc function
-contract MoodDiary {
-    // string variable to store mood
-    string public mood;
-
-    // function to set the mood use _mood as a parameter to avoid 
-    // variable overshadowing
-    function setMood(string memory _mood) public {
-        mood = _mood;
-    }
-
-    // fucntion to detect the mood from the smart contract
-    // view only function that doe sinvoke blockchain changes
-    // caller requires no gas fees to execute the fucntion to read data
-    function getMood() public view returns(string memory) {
-        return mood;
-    }
-}
+// Inheritance is the procedure by which one contract inherit the
+// the attributes and methods of another contract. SOlidity supports
+//  multiple inheritance. Contracts can inherit other contract by
+// by using the 'is' keyword
+// 
+// A parent contract which has a function that can be overridden
+// by a child contract must be declared as a 'virtual function'.
+// 
+// A child contract that is going to override a parent function
+// must use the 'override' keyword.
+// The order of inheritance matters if parent contracts share methods
+// or attributes by the same name
+contract A {
+   
