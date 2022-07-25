a0499bc3beccd38829e55ac57750ccc54ccf8b34
Alvo Rithm <alvorithmemail@gmail.com>
2022-07-25 15:43:59 +0200

    initial commit for advanced solidity run through: lesson 1 completed

---
+++ 2.Sophomore_track/1.0.Advanced-Solidity/1.0.Mapping.sol
@@ -0,0 +1,29 @@
+// SPX-License-Identifier: MIT
+pragma solidity ^0.8.10;
+
+// Mappings in solidity act like hasmhmaps or dictionaries in other programming
+// languages. They are used to store the data in key-value pairs.
+// Mappings are created using this syntax: 'mapping (keyType => valueType)'
+
+// define the contract using contratc function
+contract Mapping {
+    // string variable to store mood
+    mapping(address => uint) public myMapp;
+
+    function get(address _addr) public view returns (uint) {
+        // Mapping always returns a value
+        // If the value was not set, it will return the default value
+        //The default value for unit is 0
+        return myMapp[_addr];
+    } 
+
+    function set(address _addr, uint _i) public {
+        // Update the value at this address
+        myMapp[_addr] = _i;
+    }
+
+    function remove(address _addr) public {
+        // Reset value to the default value;
+        delete myMapp[_addr];
+    }
+    }
