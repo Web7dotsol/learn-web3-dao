98e29faabb86a1523c19689665b7dfca9983f31d
Alvo Rithm <alvorithmemail@gmail.com>
2022-07-25 22:37:33 +0200

    done with advanced solidity coding section

--- 2.Sophomore_track/1.0.Advanced-Solidity/12.0.SolidityLibraries.sol
+++ 2.Sophomore_track/1.0.Advanced-Solidity/12.0.SolidityLibraries.sol
@@ -1,21 +1,24 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.10;
+// Solidity Libraries
 
-// define the contract using contratc function
-contract MoodDiary {
-    // string variable to store mood
-    string public mood;
+// Libraries are similar to contracts in Solidity, with a few limitations. 
+// Libraries cannot contain any state variables, and cannot transfer ETH.
 
-    // function to set the mood use _mood as a parameter to avoid 
-    // variable overshadowing
-    function setMood(string memory _mood) public {
-        mood = _mood;
-    }
+// Typically, libraries are used to add helper functions to your contracts. 
+// An extremely commonly used library in Solidity world is SafeMath - which 
+// ensures that mathematical operations do not cause an integer underflow 
+// or overflow.
+contract SafeMath {
+ function add(uint x, uint y) internal pure returns (uint) {
+    uint z = x + y;
+    // if z overflowed, throw an error
+    require(z >= x, "uint overflow");
+    return z;
+ }
 
-    // fucntion to detect the mood from the smart contract
-    // view only function that doe sinvoke blockchain changes
-    // caller requires no gas fees to execute the fucntion to read data
-    function getMood() public view returns(string memory) {
-        return mood;
+ contract TestSafeMath {
+    function testAdd(uint x, uint y) public pure returns (uint) {
+        return SafeMath.add(x, y);
     }
 }
