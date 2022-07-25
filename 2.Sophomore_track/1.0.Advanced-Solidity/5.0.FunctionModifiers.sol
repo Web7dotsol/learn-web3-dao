131a3eec7340580a81ed4cf5361d08a66f9475a3
Alvo Rithm <alvorithmemail@gmail.com>
2022-07-25 17:40:12 +0200

    implemented a function modifier

--- 2.Sophomore_track/1.0.Advanced-Solidity/5.0.FunctionModifiers.sol
+++ 2.Sophomore_track/1.0.Advanced-Solidity/5.0.FunctionModifiers.sol
@@ -7,5 +7,29 @@ pragma solidity ^0.8.10;
 
 // define the contract using contratc function
 contract FunctionModifiers {
-  
+  address public owner;
+
+  constructor() {
+      // Set the contract deployer as the owner of the contract
+      owner = msg.sender;
+  }
+
+  //Create a modifier tha only allows a fucntion to be called by the owner;
+  modifier onlyOwner() {
+    require(msg.sender == owner, "You are not the owner");
+
+    //Underscore is a special character used inside modifiers
+    //Which tells Solidity to execute the function the modifier is used on
+    // at this point
+    // Therefore, this modifier will first perform the above check
+    // Then run the rest of the code
+    _;
+  }
+
+ // Create a function and apply the onlyOwner modifier on it
+ function changeOwner(address _newowner) public onlyOwner {
+     // We will only reach this point if the modifer succeeds with its checks
+     // So the caller of this transaction must be the current owener
+     owner = _newowner;
+ }
 }
