11cb63fbd177cc189c641e51895f156820f74ac8
Alvo Rithm <alvorithmemail@gmail.com>
2022-07-25 22:04:14 +0200

    local imports lesson completed

--- 2.Sophomore_track/1.0.Advanced-Solidity/11.0.LocalImports-1.sol
+++ 2.Sophomore_track/1.0.Advanced-Solidity/11.0.LocalImports-1.sol
@@ -1,21 +1,18 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.10;
 
+// Import Statements
+// To maintain code readability, you can split your Solidity code over
+// multiple files. Solidity allows importing both local and external files.
+
+// Local Imports
+// Assume we have a folder structure like this:
+
+// ├── 11.0.LocalImports-2.sol
+// └── 11.0.LocalImports-1.sol
+// where 11.0.LocalImports-1.sol is
+
 // define the contract using contratc function
-contract MoodDiary {
-    // string variable to store mood
-    string public mood;
-
-    // function to set the mood use _mood as a parameter to avoid 
-    // variable overshadowing
-    function setMood(string memory _mood) public {
-        mood = _mood;
-    }
-
-    // fucntion to detect the mood from the smart contract
-    // view only function that doe sinvoke blockchain changes
-    // caller requires no gas fees to execute the fucntion to read data
-    function getMood() public view returns(string memory) {
-        return mood;
-    }
+contract Foo {
+   string public name = "Foo";
 }
--- 2.Sophomore_track/1.0.Advanced-Solidity/11.1.LocalImports-2.sol
+++ 2.Sophomore_track/1.0.Advanced-Solidity/11.1.LocalImports-2.sol
@@ -1,21 +1,33 @@
-// SPX-License-Identifier: MIT
+// SPDX-License-Identifier: MIT
 pragma solidity ^0.8.10;
 
-// define the contract using contratc function
-contract MoodDiary {
-    // string variable to store mood
-    string public mood;
+// Import Statements
+// To maintain code readability, you can split your Solidity code over
+// multiple files. Solidity allows importing both local and external files.
 
-    // function to set the mood use _mood as a parameter to avoid 
-    // variable overshadowing
-    function setMood(string memory _mood) public {
-        mood = _mood;
-    }
+// Local Imports
+// Assume we have a folder structure like this:
 
-    // fucntion to detect the mood from the smart contract
-    // view only function that doe sinvoke blockchain changes
-    // caller requires no gas fees to execute the fucntion to read data
-    function getMood() public view returns(string memory) {
-        return mood;
+// ├── 11.1.LocalImports-2.sol
+// └── 11.0.LocalImports-1.sol
+// we can import 11.0.LocalImports-1.sol and use it in 11.1.LocalImports-1.sol 
+// as such
+
+  // import 11.0.LocalImports-1.sol
+  import "./11.0.LocalImports-1.sol";
+
+  contract Import {
+    // Initialize 11.0.LocalImports-1.sol
+    Foo public foo = new LocalImports1();
+
+    //Test 11.0.LocalImports-1.sol by getting it's name.
+    function getFooName() public view returns (string memory) {
+        return foo.name();
     }
-}
+  }
+  
+//   Added_Info: When we use Hardhat, we can also install contracts as 
+//   node modules through npm, and then import contracts from the node_modules 
+//   folder. These also count as local imports, as technically when you install 
+//   a package you are downloading the contracts to your local machine.
+
